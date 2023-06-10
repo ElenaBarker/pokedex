f9517a36565b0beab7015ae5fe3efe2e7054b322
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-06-10 04:01:43 +0100

    almost all done (props needs to be done)

---
+++ src/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Logo = () => {
+  const appName = "[YOUR_NAME]'s Pokedex";
+  return (
+    <header>
+      <h1>Welcome to the {appName}</h1>
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="Bird"
+      ></img>
+    </header>
+  );
+};
+
+
+
+export default Logo;
+
